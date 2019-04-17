2f09cd10571c23c76c5d3cd04331342f6b5a4532
Florian GOTO <florian.goto-ext@continental-corporation.com>
2019-04-17 16:29:40 +0200

    update function closures

--- function-closures/function-closures.go
+++ function-closures/function-closures.go
@@ -8,7 +8,6 @@ import "fmt"
 func adder() func(int) int {
 	sum := 0
 	// returns a closure, the func is bound to the sum variable
-	// Each closure is bound to its own sum variable.
 	return func(x int) int {
 		sum += x
 		return sum
@@ -16,6 +15,7 @@ func adder() func(int) int {
 }
 
 func main() {
+	// Each closure is bound to its own sum variable.
 	pos, neg := adder(), adder()
 	for i := 0; i < 10; i++ {
 		fmt.Println(
