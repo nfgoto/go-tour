4417267bdf2d0f0bd1404e2b9678d17a61d42870
Florian GOTO <florian.goto-ext@continental-corporation.com>
2019-04-19 09:00:35 +0200

    update method continued

--- methods-continued/methods-continued.go
+++ methods-continued/methods-continued.go
@@ -5,11 +5,14 @@ import (
 	"math"
 )
 
+// You can declare a method on NON-STRUCT types
+// can only declare methods on types defined in the SAME PACKAGE as method
+// to add methods on types not defined in same package, create a new type pointing to the other
+
 // numeric type
 type MyFloat float64
 
 // method on a numeric type
-// can only declare methods on types defined in the same package as method
 func (f MyFloat) Abs() float64 {
 	if f < 0 {
 		return float64(-f)
