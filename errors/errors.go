259710993de980b1790c51f112b51d65952e2a89
Florian GOTO <florian.goto-ext@continental-corporation.com>
2019-04-23 13:34:54 +0200

    update errors

--- errors/errors.go
+++ errors/errors.go
@@ -1,3 +1,19 @@
+/*
+	Go programs express error state with error values.
+
+	The error type is a built-in interface similar to fmt.Stringer:
+
+	type error interface {
+		Error() string
+	}
+
+	Functions often return an error value, and calling code should handle errors by testing whether the error equals nil.
+
+	A nil error denotes success; a non-nil error denotes failure.
+
+
+
+*/
 package main
 
 import (
