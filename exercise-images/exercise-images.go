ebf47213ae7207802d635f74e76c21ede3bda3c2
Florian GOTO <florian.goto-ext@continental-corporation.com>
2019-04-23 14:54:01 +0200

    add image &&  exercise

---
+++ exercise-images/exercise-images.go
@@ -0,0 +1,45 @@
+/*
+	Remember the picture generator you wrote earlier? Let's write another one, but this time it will return an implementation of image.Image instead of a slice of data.
+
+Define your own Image type, implement the necessary methods, and call pic.ShowImage.
+
+Bounds should return a image.Rectangle, like image.Rect(0, 0, w, h).
+
+ColorModel should return color.RGBAModel.
+
+At should return a color; the value v in the last picture generator corresponds to color.RGBA{v, v, 255, 255} in this one.
+
+*/
+
+// +build ignore
+
+package main
+
+import (
+	"image"
+	"image/color"
+
+	"golang.org/x/tour/pic"
+)
+
+type Image struct {
+	Height, Width int
+}
+
+func (m Image) ColorModel() color.Model {
+	return color.RGBAModel
+}
+
+func (m Image) Bounds() image.Rectangle {
+	return image.Rect(0, 0, m.Height, m.Width)
+}
+
+func (m Image) At(x, y int) color.Color {
+	c := uint8(x ^ y)
+	return color.RGBA{c, c, 255, 255}
+}
+
+func main() {
+	m := Image{256, 256}
+	pic.ShowImage(m)
+}
