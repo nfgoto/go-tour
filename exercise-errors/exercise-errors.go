58e5c141cdb7e290a9ac2886eba53fe7f2dc96cd
Florian GOTO <florian.goto-ext@continental-corporation.com>
2019-04-23 13:58:07 +0200

    update errors exercise

--- exercise-errors/exercise-errors.go
+++ exercise-errors/exercise-errors.go
@@ -10,21 +10,22 @@ const delta = 1e-6
 type ErrNegativeSqrt float64
 
 func (e ErrNegativeSqrt) Error() string {
+	// need to convert ErrNegativeSqrt type to the underlying float64 type to avoid infinite loop
 	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
 }
 
 func Sqrt(x float64) (float64, error) {
 	z := x
 
-	if x > 0 {
-		n := 0.0
-		for math.Abs(n-z) > delta {
-			n, z = z, z-(z*z-x)/(2*z)
-		}
-		return z, nil
+	if x < 0 {
+		return 0, ErrNegativeSqrt(z)
 	}
 
-	return 0, ErrNegativeSqrt(z)
+	n := 0.0
+	for math.Abs(n-z) > delta {
+		n, z = z, z-(z*z-x)/(2*z)
+	}
+	return z, nil
 }
 
 func main() {
