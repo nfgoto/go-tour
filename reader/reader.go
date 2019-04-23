f0abb30ced9aaebfa83bb67fbe0e2b46a73e3e50
Florian GOTO <florian.goto-ext@continental-corporation.com>
2019-04-23 14:20:44 +0200

    add reader

---
+++ reader/reader.go
@@ -0,0 +1,30 @@
+/*
+	The io package specifies the io.Reader interface,
+	which represents the read end of a stream of data.
+
+	Read populates the given byte slice with data and
+	returns the number of bytes populated and an error value.
+	It returns an io.EOF error when the stream ends.
+
+*/
+package main
+
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
+func main() {
+	r := strings.NewReader("Hello, Reader!")
+
+	b := make([]byte, 8)
+	for {
+		n, err := r.Read(b)
+		fmt.Printf("n = %v err = %v b = %v\n", n, err, b)
+		fmt.Printf("b[:n] = %q\n", b[:n])
+		if err == io.EOF {
+			break
+		}
+	}
+}
